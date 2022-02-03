ab5e07a9c09f0616176490653a4a1e550a135cc1
Kloska <56342396+Kloska@users.noreply.github.com>
2022-02-03 13:38:55 +0100

    start

--- src/com/company/Crypt.java
+++ src/com/company/Crypt.java
@@ -1,54 +1,22 @@
 package com.company;
-private char[] letters;
 
 public class Crypt {
-    public static void main(String[] args) {
-        encrypt();
-        exempel2();
-        exempel3();
+    static String value = "SampleStringToBeEncrypted";
+    static String keyval = "thisIsAKey";
+
+    public static void main(String args[]) { // test
+        int[] encrypted = encrypt(value,keyval);
+        for(int i = 0; i < encrypted.length; i++)
+            System.out.printf("%d,", encrypted[i]);
+        System.out.println("");
     }
 
-    // Visar hur exemplet i teorigenomgången funkar i Java
-    private static void encrypt() {
-        String message = "Hej";
-
-        letters = message.toCharArray();
-        char key = '1';
-        String crypt = "";
-
-        for(int i =0;i<letters.length;i++) {
-
-            for(int j =0;j<list.size();j++) {
-                if(letters[i]==list.get(j)) {
-                    letters[i]=shuffledList.get(j);
-                    break;
-                }
-
-       /* for (int i = 0 ; i < message.length() ; i++) {
-            System.out.println(Integer.toBinaryString(message.charAt(i)));
-            System.out.println("0" + Integer.toBinaryString(key));
-            System.out.println(Integer.toBinaryString(message.charAt(i)^key));
-            System.out.println((message.charAt(i)^key) + "\n");
-            crypt += (char)(message.charAt(i)^key);
-        } */
-        System.out.println(crypt);
-    }
-
-    // Visar hur XOR-kryptering funkar på byte-nivå
-    public static void exempel2() {
-        byte A = 72;
-        byte B = 40;
-        // måste casta till byte eftersom det alltid blir en int
-        byte C = (byte)(A^B);
-        System.out.println(A + "^" + B + "=" +C);
-    }
-
-    // Visar hur XOR-kryptering funkar på char-nivå
-    public static void exempel3() {
-        char m = 'H';
-        char k = '(';
-        // måste casta till char eftersom det alltid blir en int
-        char c = (char)(m^k);
-        System.out.println(m + "^" + k + "=" + c);
+    private static int[] encrypt(String str, String key) {
+        int[] output = new int[str.length()];
+        for(int i = 0; i < str.length(); i++) {
+            int o = (Integer.valueOf(str.charAt(i)) ^ Integer.valueOf(key.charAt(i % (key.length() - 1)))) + '0';
+            output[i] = o;
+        }
+        return output;
     }
 }
